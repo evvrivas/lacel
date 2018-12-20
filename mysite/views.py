a0ec95496759d4b6b2a328f21caa2317ce037795
evvrivas <evvrivas@gmail.com>
2018-12-20 17:23:24 -0600

    gel

--- mysite/views.py
+++ mysite/views.py
@@ -134,70 +134,203 @@ def datos_prueba(request):
 
     date=datetime.datetime.now()
 
-    p1=Centrales(nombre="5 NOVIEMBRE",fecha_ingreso=date)   
+
+
+    p1=Centrales(nombre="CH 5 NOVIEMBRE",fecha_ingreso=date)   
     p1.save() 
 
 
-    p21=Transformadores(central=p1,codigo="0001",marca="abbax",modelo="2121a",cararcteristicas="230v ac",fecha_ingreso=date)
+    p21=Transformadores(central=p1,codigo="C0848A",marca="EFACEC",modelo="NA",cararcteristicas="U1",fecha_ingreso=date)
     p21.save()
-
-    p22=Transformadores(central=p1,codigo="0002",marca="xxxxx",modelo="xxxxx2",cararcteristicas="230v ac",fecha_ingreso=date)
-    p22.save()
-
-    p23=Transformadores(central=p1,codigo="0003",marca="yyyyy",modelo="xxxxx3",cararcteristicas="230v ac",fecha_ingreso=date)
-    p23.save()
- 
-
-    p31=Mediciones(central=p1,transformador=p21,codigo_usuario="7807004",Hidrogeno=1,Oxigeno=2,Nitrogeno=6,Metano=1,Monoxido_de_carbono=63,Etano=2,Dioxido_de_carbono=17,Etileno=7,Acetileno=8,Propileno=9,Propano=0,Butano=61,fecha_ingreso=date,fecha_del_analisis=date)
+      
+    #EFACET U1
+    #2014
+    p31=Mediciones(central=p1,transformador=p21,codigo_usuario="7807004",Hidrogeno=3,Oxigeno=0,Nitrogeno=0,Metano=1,Monoxido_de_carbono=4,Etano=1,Dioxido_de_carbono=203.41,Etileno=0.17,Acetileno=0.7,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p31.save()
-
-    p32=Mediciones(central=p1,transformador=p21,codigo_usuario="7807004",Hidrogeno=5,Oxigeno=8,Nitrogeno=3,Metano=2,Monoxido_de_carbono=44,Etano=6,Dioxido_de_carbono=7,Etileno=7,Acetileno=45,Propileno=3,Propano=30,Butano=1,fecha_ingreso=date,fecha_del_analisis=date)
+    #2015
+    p32=Mediciones(central=p1,transformador=p21,codigo_usuario="7807004",Hidrogeno=5.38,Oxigeno=29.92,Nitrogeno=36320,Metano=8.8,Monoxido_de_carbono=4.97,Etano=2.1,Dioxido_de_carbono=926,Etileno=0.68,Acetileno=1.2,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p32.save()
-
-    p33=Mediciones(central=p1,transformador=p22,codigo_usuario="7807004",Hidrogeno=6,Oxigeno=6,Nitrogeno=87,Metano=14,Monoxido_de_carbono=4,Etano=112,Dioxido_de_carbono=47,Etileno=37,Acetileno=84,Propileno=9,Propano=0,Butano=21,fecha_ingreso=date,fecha_del_analisis=date)
+    #2016
+    p33=Mediciones(central=p1,transformador=p21,codigo_usuario="7807004",Hidrogeno=2,Oxigeno=0,Nitrogeno=0,Metano=50,Monoxido_de_carbono=137,Etano=67,Dioxido_de_carbono=2010,Etileno=5,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p33.save()
-
-    p34=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=7,Oxigeno=3,Nitrogeno=33,Metano=34,Monoxido_de_carbono=53,Etano=6,Dioxido_de_carbono=73,Etileno=65,Acetileno=5,Propileno=90,Propano=4,Butano=12,fecha_ingreso=date,fecha_del_analisis=date)
+    #2017
+    p34=Mediciones(central=p1,transformador=p21,codigo_usuario="7807004",Hidrogeno=5,Oxigeno=0,Nitrogeno=0,Metano=57,Monoxido_de_carbono=155,Etano=79,Dioxido_de_carbono=2037,Etileno=10,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p34.save()
-
-    p35=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=6,Oxigeno=12,Nitrogeno=23,Metano=42,Monoxido_de_carbono=25,Etano=6,Dioxido_de_carbono=27,Etileno=7,Acetileno=7,Propileno=19,Propano=60,Butano=1,fecha_ingreso=date,fecha_del_analisis=date)
+    #2018
+    p35=Mediciones(central=p1,transformador=p21,codigo_usuario="7807004",Hidrogeno=15,Oxigeno=0,Nitrogeno=0,Metano=89,Monoxido_de_carbono=159,Etano=134,Dioxido_de_carbono=2308,Etileno=12,Acetileno=3,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p35.save()
 
-    p36=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=10,Oxigeno=21,Nitrogeno=31,Metano=4,Monoxido_de_carbono=5,Etano=6,Dioxido_de_carbono=74,Etileno=44,Acetileno=8,Propileno=9,Propano=0,Butano=1,fecha_ingreso=date,fecha_del_analisis=date)
-    p36.save()
-
-    p1=Centrales(nombre="GUAJOYO",fecha_ingreso=date)   
-    p1.save() 
 
 
-    p21=Transformadores(central=p1,codigo="000a",marca="abbax",modelo="2121a",cararcteristicas="230v ac",fecha_ingreso=date)
-    p21.save()
 
-    p22=Transformadores(central=p1,codigo="000b",marca="xxxxx",modelo="xxxxx2",cararcteristicas="230v ac",fecha_ingreso=date)
+
+    p22=Transformadores(central=p1,codigo="33562",marca="TUBOS TRRANS ELECTRIC",modelo="NA",cararcteristicas="U2",fecha_ingreso=date)
     p22.save()
-
-    p23=Transformadores(central=p1,codigo="000c",marca="yyyyy",modelo="xxxxx3",cararcteristicas="230v ac",fecha_ingreso=date)
-    p23.save()
- 
- 
-
-    p31=Mediciones(central=p1,transformador=p21,codigo_usuario="7807002",Hidrogeno=1,Oxigeno=2,Nitrogeno=6,Metano=1,Monoxido_de_carbono=63,Etano=2,Dioxido_de_carbono=17,Etileno=7,Acetileno=8,Propileno=9,Propano=0,Butano=61,fecha_ingreso=date,fecha_del_analisis=date)
+    #2010
+    p31=Mediciones(central=p1,transformador=p22,codigo_usuario="7807004",Hidrogeno=5,Oxigeno=0,Nitrogeno=0,Metano=4,Monoxido_de_carbono=132,Etano=12,Dioxido_de_carbono=2027,Etileno=25,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p31.save()
-
-    p32=Mediciones(central=p1,transformador=p21,codigo_usuario="7807002",Hidrogeno=5,Oxigeno=8,Nitrogeno=3,Metano=2,Monoxido_de_carbono=44,Etano=6,Dioxido_de_carbono=7,Etileno=7,Acetileno=45,Propileno=3,Propano=30,Butano=1,fecha_ingreso=date,fecha_del_analisis=date)
+    #2012
+    p32=Mediciones(central=p1,transformador=p22,codigo_usuario="7807004",Hidrogeno=24,Oxigeno=0,Nitrogeno=0,Metano=7,Monoxido_de_carbono=352,Etano=10,Dioxido_de_carbono=3641,Etileno=34,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p32.save()
-
-    p33=Mediciones(central=p1,transformador=p22,codigo_usuario="7807002",Hidrogeno=6,Oxigeno=6,Nitrogeno=87,Metano=14,Monoxido_de_carbono=4,Etano=112,Dioxido_de_carbono=47,Etileno=37,Acetileno=84,Propileno=9,Propano=0,Butano=21,fecha_ingreso=date,fecha_del_analisis=date)
+    #2013
+    p33=Mediciones(central=p1,transformador=p22,codigo_usuario="7807004",Hidrogeno=17,Oxigeno=0,Nitrogeno=87500,Metano=9,Monoxido_de_carbono=463,Etano=11,Dioxido_de_carbono=4140,Etileno=40,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p33.save()
-
-    p34=Mediciones(central=p1,transformador=p23,codigo_usuario="7807002",Hidrogeno=7,Oxigeno=3,Nitrogeno=33,Metano=34,Monoxido_de_carbono=53,Etano=6,Dioxido_de_carbono=73,Etileno=65,Acetileno=5,Propileno=90,Propano=4,Butano=12,fecha_ingreso=date,fecha_del_analisis=date)
+    #2014
+    p34=Mediciones(central=p1,transformador=p22,codigo_usuario="7807004",Hidrogeno=16,Oxigeno=0,Nitrogeno=0,Metano=4,Monoxido_de_carbono=42,Etano=7,Dioxido_de_carbono=680,Etileno=11,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p34.save()
-
-    p35=Mediciones(central=p1,transformador=p23,codigo_usuario="7807002",Hidrogeno=6,Oxigeno=12,Nitrogeno=23,Metano=42,Monoxido_de_carbono=25,Etano=6,Dioxido_de_carbono=27,Etileno=7,Acetileno=7,Propileno=19,Propano=60,Butano=1,fecha_ingreso=date,fecha_del_analisis=date)
+    #2015
+    p35=Mediciones(central=p1,transformador=p22,codigo_usuario="7807004",Hidrogeno=12,Oxigeno=0,Nitrogeno=0,Metano=11,Monoxido_de_carbono=228,Etano=3,Dioxido_de_carbono=2226,Etileno=5,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
     p35.save()
 
-    p36=Mediciones(central=p1,transformador=p23,codigo_usuario="7807002",Hidrogeno=10,Oxigeno=21,Nitrogeno=31,Metano=4,Monoxido_de_carbono=5,Etano=6,Dioxido_de_carbono=74,Etileno=44,Acetileno=8,Propileno=9,Propano=0,Butano=1,fecha_ingreso=date,fecha_del_analisis=date)
-    p36.save()
+    #2016
+    p35=Mediciones(central=p1,transformador=p22,codigo_usuario="7807004",Hidrogeno=15,Oxigeno=0,Nitrogeno=0,Metano=9,Monoxido_de_carbono=247,Etano=9,Dioxido_de_carbono=2381,Etileno=14,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p35.save()
+
+
+
+
+
+
+    p23=Transformadores(central=p1,codigo="L252373",marca="GENERAL ELECTRIC",modelo="NA",cararcteristicas="U3",fecha_ingreso=date)
+    p23.save()
+
+     #2012
+    p31=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=106,Oxigeno=0,Nitrogeno=0,Metano=17,Monoxido_de_carbono=552,Etano=8,Dioxido_de_carbono=6920,Etileno=34,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p31.save()
+    #2013
+    p32=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=74,Oxigeno=0,Nitrogeno=0,Metano=14,Monoxido_de_carbono=553,Etano=6,Dioxido_de_carbono=6798,Etileno=32,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p32.save()
+    #2014
+    p33=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=74,Oxigeno=727,Nitrogeno=65942,Metano=12,Monoxido_de_carbono=427,Etano=3,Dioxido_de_carbono=4351,Etileno=17,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p33.save()
+    #2015
+    p34=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=43,Oxigeno=0,Nitrogeno=0,Metano=8,Monoxido_de_carbono=334,Etano=8,Dioxido_de_carbono=4162,Etileno=18,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p34.save()
+    #2016
+    p35=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=46,Oxigeno=0,Nitrogeno=0,Metano=9,Monoxido_de_carbono=330,Etano=8,Dioxido_de_carbono=4320,Etileno=17,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p35.save()
+    #2017
+    p35=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=5,Oxigeno=0,Nitrogeno=0,Metano=1,Monoxido_de_carbono=9,Etano=2,Dioxido_de_carbono=151,Etileno=0,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p35.save()
+     #2018
+    p35=Mediciones(central=p1,transformador=p23,codigo_usuario="7807004",Hidrogeno=11,Oxigeno=0,Nitrogeno=0,Metano=4,Monoxido_de_carbono=177,Etano=5,Dioxido_de_carbono=1896,Etileno=5,Acetileno=3,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p35.save()
+
+
+
+
+
+    p24=Transformadores(central=p1,codigo="89115",marca="TADEO CZERWENY",modelo="NA",cararcteristicas="U4",fecha_ingreso=date)
+    p24.save()
+
+     #2012
+    p31=Mediciones(central=p1,transformador=p24,codigo_usuario="7807004",Hidrogeno=36,Oxigeno=0,Nitrogeno=0,Metano=22,Monoxido_de_carbono=522,Etano=6,Dioxido_de_carbono=1953,Etileno=9,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p31.save()
+    #2013
+    p32=Mediciones(central=p1,transformador=p24,codigo_usuario="7807004",Hidrogeno=24,Oxigeno=0,Nitrogeno=0,Metano=29,Monoxido_de_carbono=708,Etano=7,Dioxido_de_carbono=2512,Etileno=8,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p32.save()
+    #2014
+    p33=Mediciones(central=p1,transformador=p24,codigo_usuario="7807004",Hidrogeno=34,Oxigeno=0,Nitrogeno=0,Metano=29,Monoxido_de_carbono=771,Etano=3,Dioxido_de_carbono=2633,Etileno=10,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p33.save()
+    #2015
+    p34=Mediciones(central=p1,transformador=p24,codigo_usuario="7807004",Hidrogeno=36,Oxigeno=0,Nitrogeno=0,Metano=32,Monoxido_de_carbono=785,Etano=3,Dioxido_de_carbono=2745,Etileno=8,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p34.save()
+    #2016
+    p35=Mediciones(central=p1,transformador=p24,codigo_usuario="7807004",Hidrogeno=26,Oxigeno=0,Nitrogeno=0,Metano=33,Monoxido_de_carbono=916,Etano=8,Dioxido_de_carbono=2978,Etileno=12,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p35.save()
+    #2017
+    p35=Mediciones(central=p1,transformador=p24,codigo_usuario="7807004",Hidrogeno=0,Oxigeno=0,Nitrogeno=0,Metano=30,Monoxido_de_carbono=1006,Etano=7,Dioxido_de_carbono=3281,Etileno=8,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p35.save()
+
+
+
+
+
+    p25=Transformadores(central=p1,codigo="E-74470689",marca="DELTA STAR",modelo="NA",cararcteristicas="U5",fecha_ingreso=date)
+    p25.save()
+
+    #2012
+    p31=Mediciones(central=p1,transformador=p25,codigo_usuario="7807004",Hidrogeno=30,Oxigeno=0,Nitrogeno=0,Metano=11,Monoxido_de_carbono=328,Etano=23,Dioxido_de_carbono=15922,Etileno=14,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p31.save()
+    #2013
+    p32=Mediciones(central=p1,transformador=p25,codigo_usuario="7807004",Hidrogeno=21,Oxigeno=0,Nitrogeno=0,Metano=13,Monoxido_de_carbono=308,Etano=20,Dioxido_de_carbono=15521,Etileno=10,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p32.save()
+    #2014
+    p33=Mediciones(central=p1,transformador=p25,codigo_usuario="7807004",Hidrogeno=18,Oxigeno=0,Nitrogeno=0,Metano=9,Monoxido_de_carbono=194,Etano=15,Dioxido_de_carbono=4940,Etileno=8,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p33.save()
+    #2015
+    p34=Mediciones(central=p1,transformador=p25,codigo_usuario="7807004",Hidrogeno=10,Oxigeno=0,Nitrogeno=112000,Metano=7,Monoxido_de_carbono=219,Etano=3,Dioxido_de_carbono=8150,Etileno=3,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p34.save()
+    #2016
+    p35=Mediciones(central=p1,transformador=p25,codigo_usuario="7807004",Hidrogeno=16,Oxigeno=0,Nitrogeno=0,Metano=1,Monoxido_de_carbono=234,Etano=35,Dioxido_de_carbono=8630,Etileno=10,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p35.save()
+
+
+
+
+
+
+
+
+
+
+
+    p26=Transformadores(central=p1,codigo="A15051",marca="TOSHIBA",modelo="NA",cararcteristicas="U6",fecha_ingreso=date)
+    p26.save()
+
+     #2016
+    p31=Mediciones(central=p1,transformador=p26,codigo_usuario="7807004",Hidrogeno=10,Oxigeno=7760,Nitrogeno=26500,Metano=5,Monoxido_de_carbono=212,Etano=2,Dioxido_de_carbono=677,Etileno=2,Acetileno=2,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p31.save()
+    #2018
+    p32=Mediciones(central=p1,transformador=p26,codigo_usuario="7807004",Hidrogeno=18,Oxigeno=0,Nitrogeno=0,Metano=29,Monoxido_de_carbono=785,Etano=3,Dioxido_de_carbono=4062,Etileno=6,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p32.save()
+
+
+
+
+
+
+
+    p27=Transformadores(central=p1,codigo="A15052",marca="TOSHIBA",modelo="NA",cararcteristicas="U7",fecha_ingreso=date)
+    p27.save()
+
+    #2016
+    p31=Mediciones(central=p1,transformador=p27,codigo_usuario="7807004",Hidrogeno=10,Oxigeno=7760,Nitrogeno=26500,Metano=5,Monoxido_de_carbono=212,Etano=2,Dioxido_de_carbono=677,Etileno=2,Acetileno=2,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p31.save()
+    #2018
+    p32=Mediciones(central=p1,transformador=p27,codigo_usuario="7807004",Hidrogeno=21,Oxigeno=0,Nitrogeno=0,Metano=32,Monoxido_de_carbono=887,Etano=6,Dioxido_de_carbono=4395,Etileno=8,Acetileno=3,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p32.save()
+
+
+
+
+
+
+    p28=Transformadores(central=p1,codigo="544328",marca="MITSUBICHI",modelo="NA",cararcteristicas="RESERVA",fecha_ingreso=date)
+    p28.save()
+
+    #2012
+    p31=Mediciones(central=p1,transformador=p28,codigo_usuario="7807004",Hidrogeno=48,Oxigeno=0,Nitrogeno=0,Metano=2,Monoxido_de_carbono=12,Etano=61,Dioxido_de_carbono=4166,Etileno=19,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p31.save()
+    #2013
+    p32=Mediciones(central=p1,transformador=p28,codigo_usuario="7807004",Hidrogeno=33,Oxigeno=0,Nitrogeno=0,Metano=3,Monoxido_de_carbono=11,Etano=60,Dioxido_de_carbono=3999,Etileno=6,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p32.save()
+    #2014
+    p33=Mediciones(central=p1,transformador=p28,codigo_usuario="7807004",Hidrogeno=8,Oxigeno=250,Nitrogeno=82.065,Metano=8,Monoxido_de_carbono=14,Etano=53,Dioxido_de_carbono=4781,Etileno=0,Acetileno=0,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p33.save()
+    #2015
+    p34=Mediciones(central=p1,transformador=p28,codigo_usuario="7807004",Hidrogeno=30,Oxigeno=0,Nitrogeno=89.8,Metano=4.9,Monoxido_de_carbono=4.9,Etano=44,Dioxido_de_carbono=4900,Etileno=1.9,Acetileno=1.9,Propileno=0,Propano=0,Butano=0,fecha_ingreso=date,fecha_del_analisis=date)
+    p34.save()
+
+    
+
+
+
+
+   
+
+
+   
 
     return render(request,'principal.html',locals())
 
@@ -232,7 +365,6 @@ def grafico_tendencias(request,central_x,transformador_x,gas_analizar):
     graphIMG = PIL.Image.fromstring('RGB', canvas.get_width_height(), canvas.tostring_rgb())
     graphIMG.save(buffer, "PNG")
     pylab.close()
-
     
     return HttpResponse (buffer.getvalue(), content_type="Image/png")
 
@@ -330,6 +462,13 @@ def limite_concentracion(request):
         ESTADO_DE_GASES.append(estado)
     #return estado_trafo,NOMBRE_GAS_PRUEBA,ESTADO_DE_GASES
     return render(request,'analisis.html',locals())
+
+
+
+
+
+
+
 
 def informacion(request):
   return render(request,'informacion.html',locals())
