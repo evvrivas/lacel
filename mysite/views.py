c203c5fb1300e4565866870997dfd43d1523070f
evvrivas <evvrivas@gmail.com>
2019-01-02 00:52:42 -0600

    si

--- mysite/views.py
+++ mysite/views.py
@@ -354,7 +354,7 @@ def datos_prueba(request):
 
     u1=User.objects.create_user(username="7807004", password="4690",email="evvaldez@cel.gob.sv",first_name="Ernesto Vladimir",last_name="Valdez Rivas")
     u1.save()
-    u1=Usuarios(codigo_usuario="7807004", pasword="4690",email="evvaldez@cel.gob.sv",nombres="Ernesto Vladimir",apellidos="Valdez Rivas",privilegio="DE_BAJA",fecha_ingreso=date)
+    u1=Usuarios(codigo_usuario="7807004", pasword="4690",email="evvaldez@cel.gob.sv",nombres="Ernesto Vladimir",apellidos="Valdez Rivas",privilegio="DE_ALTA",fecha_ingreso=date)
     u1.save()
 
 
