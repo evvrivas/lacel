8cf53f156297fc0c862761d80b2684e8ebbdb365
evvrivas <evvrivas@gmail.com>
2019-05-28 08:37:42 -0600

    BUALA

--- mysite/datos_artetronica/admin.py
+++ mysite/datos_artetronica/admin.py
@@ -16,11 +16,9 @@ class RulesAdmin(admin.ModelAdmin):
 class UsuariosAdmin(admin.ModelAdmin):
     model = Usuarios
     list_display = ['central_nombre','codigo_usuario', 'nombres','apellidos','fecha_ingreso',]
-	def central_nombre(self,instance):
-		return instance.central.nombre
-
-
-admin.site.register(Usuarios,UsuariosAdmin)
+    def central_nombre(self,instance):
+    	return instance.central.nombre
+    admin.site.register(Usuarios,UsuariosAdmin)
 ##############################################
 
 
@@ -41,8 +39,8 @@ class TransformadoresAdmin(admin.ModelAdmin):
     model = Transformadores
     list_display = ['central_nombre','codigo', 'marca','modelo',]
     def central_nombre(self,instance):
-		return instance.central.nombre
-admin.site.register(Transformadores,TransformadoresAdmin)
+    	return instance.central.nombre
+    admin.site.register(Transformadores,TransformadoresAdmin)
 
 ##############################################
 
@@ -54,8 +52,8 @@ class GeneradoresAdmin(admin.ModelAdmin):
     model = Generadores
     list_display = ['central_nombre','codigo', 'marca','modelo',]
     def central_nombre(self,instance):
-		return instance.central.nombre
-admin.site.register(Generadores,GeneradoresAdmin)
+    	return instance.central.nombre
+    admin.site.register(Generadores,GeneradoresAdmin)
 ##############################################
 
 
@@ -66,11 +64,10 @@ class MedicionesAdmin(admin.ModelAdmin):
     model = Mediciones
     list_display = ['central_nombre','transformador_marca', 'fecha_del_analisis',]
     def central_nombre(self,instance):
-		return instance.central.nombre
-	def transformador_marca(self,instance):
-		return instance.transformador.marca
-
-admin.site.register(Mediciones,MedicionesAdmin)
+    	return instance.central.nombre
+    def transformador_marca(self,instance):
+    	return instance.transformador.marca
+    admin.site.register(Mediciones,MedicionesAdmin)
 ##############################################
 
 
@@ -79,12 +76,12 @@ class RulesAdmin(admin.ModelAdmin):
     form = Mediciones_DPForm
 class Mediciones_DPAdmin(admin.ModelAdmin):
     model = Mediciones_DP
-    list_display = ['central_nombre', 'generador__marca','fecha_del_analisis',]    
+    list_display = ['central_nombre', 'generador__marca','fecha_del_analisis',]
     def central_nombre(self,instance):
-		return instance.central.nombre
-	def generador_marca(self,instance):
-		return instance.generador.marca
-admin.site.register(Mediciones_DP,Mediciones_DPAdmin)  
+    	return instance.central.nombre
+    def generador_marca(self,instance):
+    	return instance.generador.marca
+    admin.site.register(Mediciones_DP,Mediciones_DPAdmin)  
 ##############################################
 
  		 
