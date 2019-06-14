2ee250f07ee0737a5b6fb9334bc0d6eb12a0ee80
evvrivas <evvrivas@gmail.com>
2019-06-14 11:11:00 -0600

    'ya'

--- mysite/datos_artetronica/models.py
+++ mysite/datos_artetronica/models.py
@@ -70,8 +70,8 @@ class Transformadores(models.Model):
 	     codigo=models.CharField(max_length=60,blank=True)
          codigo_usuario=models.CharField(max_length=7)
          marca=models.CharField(max_length=60,blank=True)
-	     modelo=models.CharField(max_length=60,blank=True)
-	     cararcteristicas=models.TextField(blank=True)
+         modelo=models.CharField(max_length=60,blank=True)
+         cararcteristicas=models.TextField(blank=True)
 	     fecha_ingreso = models.DateField(default=datetime.now,editable = False)
 
 	     def __str__(self):
